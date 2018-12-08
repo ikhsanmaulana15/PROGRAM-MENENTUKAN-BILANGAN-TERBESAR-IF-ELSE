4c734bcea5f6a342affd67f6a5a27eebbadf3abc
IKHSAN MAULANA <45529723+ikhsanmaulana15@users.noreply.github.com>
2018-12-08 20:46:07 +0700

    Add files via upload

---
+++ bilanganterbesar.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int bil1, bil2, bil3, terbesar;
+cout<<"Masukkan bilangan 1 : ";
+ cin>>bil1;
+cout<<"Masukkan bilangan 2 : ";
+ cin>>bil2;
+cout<<"Masukkan bilangan 3 : ";
+ cin>>bil3;
+  
+{
+if(bil1 > bil2)
+terbesar = bil1;
+ 
+else
+terbesar = bil2;
+ 
+if(bil3 > terbesar)
+terbesar = bil3;
+ 
+cout<<endl;    
+cout<<"Nilai Terbesar = "<<terbesar;cout<<endl;}
+ 
+    return 0;
+}
